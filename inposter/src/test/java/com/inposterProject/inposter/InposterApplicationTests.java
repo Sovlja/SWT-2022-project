0de9f240d35aabfce5e8b0143d4b2f3f24e68d39
Sovlja <101639981+Sovlja@users.noreply.github.com>
2023-05-15 19:29:14 +0200

    Project definition

---
+++ inposter/src/test/java/com/inposterProject/inposter/InposterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inposterProject.inposter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InposterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
